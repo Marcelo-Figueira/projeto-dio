1b5dc04faf5262839e29c2035d80627ddb5efd99
marcelo-figueira <marcelofigueira2032@gmail.com>
2023-12-19 16:50:38 -0300

    projeto dio

---
+++ teste.js
@@ -0,0 +1,29 @@
+let nomeDoHeroi = "Batman"
+let nivelDoHeroi = 
+const saida1 = "O herói de nome "
+const saida2 = " está no nível "
+
+if (nivelDoHeroi <= 1000) {
+    console.log(saida1 + nomeDoHeroi + saida2 + "ferro")
+}
+else if (nivelDoHeroi <= 2001) {
+    console.log(saida1 + nomeDoHeroi + saida2 + "bronze")
+}
+else if (nivelDoHeroi <= 5001) {
+    console.log(saida1 + nomeDoHeroi + saida2 + "prata")
+}
+else if (nivelDoHeroi <= 6001) {
+    console.log(saida1 + nomeDoHeroi + saida2 + "ouro")
+}
+else if (nivelDoHeroi <= 7001) {
+    console.log(saida1 + nomeDoHeroi + saida2 + "platina")
+}
+else if (nivelDoHeroi <= 8001) {
+    console.log(saida1 + nomeDoHeroi + saida2 + "ascendente")
+}
+else if (nivelDoHeroi <= 9000) {
+    console.log(saida1 + nomeDoHeroi + saida2 + "imortal")
+}
+else if (nivelDoHeroi >= 10000) {
+    console.log(saida1 + nomeDoHeroi + saida2 + "radiante")
+}
